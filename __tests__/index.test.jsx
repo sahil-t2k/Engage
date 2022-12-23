adaa0def52289e7dc76f07abec8dd104a699a32f
sahilHussain <sahil.hussain@travel2kashmir.com>
2022-12-23 17:58:24 +0530

    user input testing

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,9 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Signin from '../pages/index'
 import '@testing-library/jest-dom'
-
+//test loading
 describe('Check if signin page loads correctly', () => {
-  
   test("Does sign-in form exist", () => {
     render(<Signin />);
    expect(screen.getByTestId('signin-form')).toBeInTheDocument();
@@ -39,6 +38,29 @@ describe('Check if signin page loads correctly', () => {
     const btns= screen.getAllByRole("button");
     expect(btns.length===5).toBeTruthy();
   })
+})
+//test validation
+describe('Check if signin page validates data correctly', () => {
+  test("To detect invalid email format",()=>{
+    render(<Signin />);
+    const mail_input=screen.getByTestId("email-field");
+    fireEvent.change(mail_input,{target:{value:'sahil@t2k'}});
+    const password_input=screen.getByTestId("password-field");
+    fireEvent.change(password_input,{target:{value:'pass@123'}});
+    const btn=screen.getByTestId('submitbtn');
+    fireEvent.click(btn);
+    expect(screen.getByText("The email field is in invalid format.")).toBeInTheDocument();
+  })
 
-
+  test("To email and password is required",()=>{
+    render(<Signin />);
+    const mail_input=screen.getByTestId("email-field");
+    fireEvent.change(mail_input,{target:{value:''}});
+    const password_input=screen.getByTestId("password-field");
+    fireEvent.change(password_input,{target:{value:''}});
+    const btn=screen.getByTestId('submitbtn');
+    fireEvent.click(btn);
+    expect(screen.getByText("The email field is required.")).toBeInTheDocument();
+    expect(screen.getByText("The password field is required")).toBeInTheDocument();
+  })
 })
