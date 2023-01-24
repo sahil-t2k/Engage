2c26fc84df30feedf1207b15580471474ea771e5
sahilHussain <sahil.hussain@travel2kashmir.com>
2023-01-24 17:56:04 +0530

    getServerSideProps implemented

--- pages/[...page].js
+++ pages/[...page].js
@@ -10,20 +10,19 @@ import Classic from "./themes/classic";
 import ClassicDark from './themes/classic-dark'
 const logger = require("../services/logger");
 var language;
-function Page() {
- 
-  const [allHotelDetails, setAllHotelDetails] = useState([]);
+function Page({ data, room_data, package_data }) {
+ const [allHotelDetails, setAllHotelDetails] = useState([]);
   const [allRooms, setAllRooms] = useState({});
   const [allPackages, setAllPackages] = useState({});
   const [phone, setPhone] = useState({});
   const [email, setEmail] = useState({});
   const [services, setServices] = useState([]);
-  const [disp,setDisp]=useState(0); 
+  const [disp, setDisp] = useState(0);
   const [theme, setTheme] = useState("Classic")
- /** Router for Redirection **/
+  /** Router for Redirection **/
   const router = useRouter();
   const fetchLanguage = (lang) => {
-    console.log("fetched language is "+lang)
+    console.log("fetched language is " + lang)
     if (lang === "ar") {
       language = ar;
     }
@@ -36,114 +35,114 @@ function Page() {
     else {
       language = en;
     }
+
+  }
+  const fetchProperty = async () => {
     
-  }
-  const fetchProperty = async (data) => {
-  var url = data;
-    console.log("url to fetch data is "+url)
-    axios.get(url)
-      .then((response) => {
-       setAllHotelDetails(response.data);
-       setTheme(response.data.theme != "" ?response.data.theme:'Classic');
-        fetchRoomDetails(response.data.property_id);
-        fetchPackageDetails(response.data.property_id);
-        response.data.contacts.map(i => { if (i.contact_type === 'Phone') { setPhone(i) } });
-        response.data.contacts.map(i => { if (i.contact_type === 'Email') { setEmail(i) } });
-        var ser =[];
-        response.data.services.map(i => { 
-          if (i.service_value !== "no") 
-          if(i.service_value !== "Not available")
-          {{
+    if(data.http_code !='404'){
+   var language= router.locale || 'en';
+    console.log("language is "+language)
+    fetchLanguage(language)
+    setAllHotelDetails(data);
+    setTheme(data.theme != "" ? data.theme : 'Classic');
+    fetchRoomDetails(room_data);
+    fetchPackageDetails(package_data);
+    data.contacts.map(i => { if (i.contact_type === 'Phone') { setPhone(i) } });
+    data.contacts.map(i => { if (i.contact_type === 'Email') { setEmail(i) } });
+    var ser = [];
+    data.services.map(i => {
+      if (i.service_value !== "no")
+        if (i.service_value !== "Not available") {
+          {
             ser.push(i)
-           } 
           }
-           setServices(ser)
-          }
-           
-           );
-        setDisp(1);
-        logger.info("url  to fetch property details hitted successfully")
-      })
-      .catch((error) => { 
-        router.push('./404');
-        logger.error("url to fetch property details, failed") });
+        }
+      setServices(ser)
+    }
+
+    );
+    setDisp(1);
+    logger.info("url  to fetch property details hitted successfully")
+  }
+  else{
+    router.push('/404');
   }
 
-  const fetchHotelDetails = async () => {
-  console.log('page is '+ router.query.page)
-  if (router?.query?.page) {
-    if(router.query.page.length===4){
-      var url;
-      var language= router.locale || 'en';
-      console.log("language is "+language)
-      fetchLanguage(language)
-     url=`/api/${router?.query?.page[0]}/${router?.query?.page[1]}/${router?.query?.page[2]}/${router?.query?.page[3]}`
-      fetchProperty(url);
-    }
-    else{
-      router.push('./404');
-     }
-      
-    }
-    else
-    {
-      console.log("waiting for router.query.page")
-    }
+  }
+
+  const fetchRoomDetails = async (room_data) => {
+      setAllRooms(room_data);
+        }
+
+  const fetchPackageDetails = async (package_data) => {
+      setAllPackages(package_data);
+      }
+
   
-  }
-  const fetchRoomDetails = async (property_id) => {
-   const url = `/api/all_rooms_details/${property_id}`;
-    axios.get(url)
-       .then((response) => {
-          setAllRooms(response.data);
-          logger.info("url  to fetch room details hitted successfully")
-         })
-       .catch((error) => { logger.error("url to fetch property details, failed") });
- }
-
- const fetchPackageDetails = async (property_id) => {
-    const url = `/api/all_packages_details/${property_id}`;
-    axios.get(url)
-       .then((response) => {
-         setAllPackages(response.data);
-          logger.info("url  to fetch package details hitted successfully")
-         })
-       .catch((error) => { logger.error("url to fetch package details, failed") });
- }
-
   /* Function call to fetch Current Property Details when page loads */
   useEffect(() => {
-    fetchHotelDetails();
-  
-  },[router.query.page]);
+    if(data==='404')
+    {
+      router.push('/404')
+    }
+    else{
+      fetchProperty();
+    }
+    
+},[data]);
 
 
-  return (<>{disp === 0?
+  return (<>{disp === 0 ?
     <>
-    <div className="h-screen bg-white">
-<div className="flex justify-center items-center h-full">
-  <img className="h-32 w-32" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt=""/>
-</div>
-</div>
-    </>:
+      <div className="h-screen bg-white">
+        <div className="flex justify-center items-center h-full">
+          <img className="h-32 w-32" src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif" alt="" />
+        </div>
+      </div>
+    </> :
     <div>
-    {/* Classic Theme */}
-    { theme === "Classic" ?
-    <div className="sticky">
-    <Classic language={language} allHotelDetails={allHotelDetails} 
-    allRooms={allRooms} allPackages={allPackages} services={services}
-    phone={phone} email={email}/></div>:<div className="sticky"></div>}
- {/* Classic Dark */}
-    { theme === "Classic-Dark" ?
-    <div className="sticky">
-    <ClassicDark language={language} allHotelDetails={allHotelDetails} 
-    allRooms={allRooms} allPackages={allPackages} services={services}
-    phone={phone} email={email}/></div>:<div className="sticky"></div>}
-</div>
-}</>
-   
+      {/* Classic Theme */}
+      {theme === "Classic" ?
+        <div className="sticky">
+          <Classic language={language} allHotelDetails={allHotelDetails}
+            allRooms={allRooms} allPackages={allPackages} services={services}
+            phone={phone} email={email} /></div> : <div className="sticky"></div>}
+      {/* Classic Dark */}
+      {theme === "Classic-Dark" ?
+        <div className="sticky">
+          <ClassicDark language={language} allHotelDetails={allHotelDetails}
+            allRooms={allRooms} allPackages={allPackages} services={services}
+            phone={phone} email={email} /></div> : <div className="sticky"></div>}
+    </div>
+  }</>
+
   );
 }
+// This gets called on every request
+export async function getServerSideProps(context) {
+  const items = context.resolvedUrl;
+  //to check if url is valid string
+  if (items.split('/').length === 5) {
+    //fetch hotel data
+    const data = await fetch(`http://103.136.36.27:7860/api${items}`)
+      .then((response) => response.json());
+    let property_id= data.property_id;
+    //fetch room data
+    const room_data=await fetch(`http://103.136.36.27:7860/api/all_rooms_details/${property_id}`)
+    .then((response) => response.json());
+//fetch package data
+    const package_data=await fetch(`http://103.136.36.27:7860/api/all_packages_details/${property_id}`)
+    .then((response) => response.json()) 
+    //return data fetched to function generation html  
+    return { props: { data,room_data,package_data } }
+  }
+  else {
+    let data = '404';
+    //if length of string is not desired then return error flag 404
+    return { props: { data } }
+  }
+}
+
 export default Page;
 Page.getLayout = function PageLayout(page) {
   return (
